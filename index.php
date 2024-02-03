09166068183290557cbf2a2d81793adb5f6a2f0e
Lakshman Karunasena <lakkaru@gmail.com>
2024-02-03 14:55:49 +0530

    feat- secondary nav, side nav and products created

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
     <title>E-commerce</title>
     <!-- bootstrap CSS links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    
+
     <!-- font awsome link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-<!-- css filese -->
-<link rel="stylesheet" href="style.css">
+    <!-- css filese -->
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -55,8 +55,113 @@
             </div>
         </nav>
 
-        <!-- last child -->
-        <div class="bg-infor p-3 text-center">All rights reserved.</div>
+        <!-- second child -->
+        <nav class="navbar navbar-expand-lg bg-secondary">
+            <ui class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Welcome Guest</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Login</a>
+                </li>
+            </ui>
+        </nav>
+
+        <!-- third child -->
+        <div class="bg-light">
+            <h3 class="text-center">Fresh Cart Express</h3>
+            <p class="text-center">Communication is at the heart of e-commerce</p>
+        </div>
+
+        <!-- fourth child -->
+        <div class="row">
+            <!-- products -->
+            <div class="col-md-10">
+                <div class="row">
+                    <div class="col-md-4 mb-2">
+                        <div class="card">
+                            <img src="./images/Bertolli.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <div class="card">
+                            <img src="./images/Bread.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <div class="card">
+                            <img src="./images/Cardini's.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-2">
+                        <div class="card">
+                            <img src="./images/Cardini's.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-info">Add to cart</a>
+                                <a href="#" class="btn btn-secondary">View more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- side nav -->
+            <div class="col-md-2 bg-secondary p-0">
+                <!-- brands -->
+            <ul class="navbar-nav me-auto text-center">
+                <li class="nav-item bg-info">
+                    <a href="#" class="nav-link text-light"><h4>Delevery Barnds</h4></a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-light">Brand 1</a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-light">Brand 2</a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-light">Brand 3</a>
+                </li>
+            </ul>
+            <!-- categories -->
+            <ul class="navbar-nav me-auto text-center">
+                <li class="nav-item bg-info">
+                    <a href="#" class="nav-link text-light"><h4>Categories</h4></a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-light">Category 1</a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-light">Category 2</a>
+                </li>
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-light">Category 3</a>
+                </li>
+            </ul>
+            </div>
+            <!-- last child -->
+            <div class="bg-infor p-3 text-center">All rights reserved.</div>
+        </div>
+
+
     </div>
 
     <!-- bootstrap ja link -->
