ef31a2c48c00a9b06189e49be3b02bd4a8c3c22b
Lakshman Karunasena <lakkaru@gmail.com>
2024-02-03 17:41:16 +0530

    feat- inserting categories and brands created

--- admin-area/index.php
+++ admin-area/index.php
@@ -9,8 +9,8 @@
     <!-- bootstrap CSS links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <!-- font awsome link
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+    <!-- font awsome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <!-- css filese -->
     <link rel="stylesheet" href="../style.css">
@@ -46,9 +46,9 @@
                 <div class="button text-center px-3">
                     <button><a href="" class="nav-link text-light bg-info m-1 p-1">Insert Product</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-1">View Products</a> </button>
-                    <button><a href="" class="nav-link text-light bg-info m-1 p-1">Insert Categories</a></button>
+                    <button><a href="index.php?insert_category" class="nav-link text-light bg-info m-1 p-1">Insert Categories</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-1">View Categories</a></button>
-                    <button><a href="" class="nav-link text-light bg-info m-1 p-1">Insert Brands</a></button>
+                    <button><a href="index.php?insert_brand" class="nav-link text-light bg-info m-1 p-1">Insert Brands</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-1">View Brands</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-1">All Orders</a></button>
                     <button><a href="" class="nav-link text-light bg-info m-1 p-1">All Payments</a></button>
@@ -58,9 +58,20 @@
             </div>
         </div>
 
+        <!-- fourth child -->
+        <div class="container my-5">
+            <?php
+            if (isset($_GET['insert_category'])) {
+                include('insert_categories.php');
+            }
+            if (isset($_GET['insert_brand'])) {
+                include('insert_brands.php');
+            }
+            ?>
+        </div>
         <!-- last child -->
         <div class="bg-infor p-3 text-center footer">All rights reserved.</div>
-        </div>
+    </div>
     </div>
 
     <!-- bootstrap ja link -->
---
+++ admin-area/insert_brands.php
@@ -0,0 +1,11 @@
+<form action="" method='post' class="mb-2">
+    <div class="input-group w-90 mb-2">
+        <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control" name="cat_title" placeholder="Insert Brand" aria-label="Insert Brand" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group w-10 mb-2 m-auto">
+        <button class="bg-info p-2 my3 border-0">Insert Brand</button>
+    </div>
+
+
+</form>
